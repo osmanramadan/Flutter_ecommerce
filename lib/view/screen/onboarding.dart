3684a0142ac5d82286ce1795005b10f2d73fc335
osmanramadan <o@o.com>
2024-03-16 20:21:19 +0200

    setup

---
+++ lib/view/screen/onboarding.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class onBoarding extends StatelessWidget {
+  const  onBoarding({Key? key}) : super(key: key);
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('osman'),
+      ),
+      body: const Center(
+        child:Text(" osman")
+        ));
+}}
